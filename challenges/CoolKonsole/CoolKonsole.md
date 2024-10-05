ab07b896137550500f3c480d0b76cceeee2f91b4
Ali Zain <alizaincodes>
2024-10-05 14:57:42 +0500

    Added CoolKonsole Challenge

---
+++ challenges/CoolKonsole/CoolKonsole.md
@@ -0,0 +1,17 @@
+# Challenge Details : CoolKonsole
+
+Make a program to display an animation of "Hello World" Text in the console.
+
+# Example Output
+<video src="exampleOutput.mp4" controls autoplay>
+
+# Hints: 
+Use `os` module for clearing the screen using `os.system("clear")`.
+Use `time` module for determing Frames Per Second(FPS). 
+
+`os.system("clear")` clears the terminal by using built-in command `clear`.
+
+This may vary with Operating Systems:
+- For Windows: `os.system("cls")`
+- For Linux: `os.system("clear")`
+- For macOS: `os.system("clear")`
--- solutions/IndexingMaster/IndexingMaster.py
+++ solutions/IndexingMaster/IndexingMaster.py
@@ -1,36 +1,6 @@
 # Solution for Challenge IndexingMaster
 
-thearray = [
-    [
-        {
-            "firstwall":{
-                "secondwall":[
-                    {
-                        "thirdwall":[
-                            {
-                                "anythingelse":[],
-                                "fourthwall":{
-                                    "fifthwall":[
-                                        {
-                                            "something":"else"
-                                        },
-                                        {
-                                            "anything":"else"
-                                        },
-                                        {
-                                            "somethingelse":"",
-                                            "hiddenwords":"dlrow olleh"
-                                        }
-                                    ]
-                                }
-                            }
-                        ]
-                    }
-                ]
-            }
-        }
-        ]
-    ]
+theData = [[{"firstwall":{"secondwall":[{"thirdwall":[{"anythingelse":[],"fourthwall":{"fifthwall":[{"something":"else"},{"anything":"else"},{"somethingelse":"","hiddenwords":"dlrow olleh"}]}}]}]}}]]
 
-thehiddenword = thearray[0][0]["firstwall"]["secondwall"][0]["thirdwall"][0]["fourthwall"]["fifthwall"][2]["hiddenwords"][::-1]
-print(thehiddenword)
+theHiddenWord = theData[0][0]["firstwall"]["secondwall"][0]["thirdwall"][0]["fourthwall"]["fifthwall"][2]["hiddenwords"][::-1]
+print(theHiddenWord)
