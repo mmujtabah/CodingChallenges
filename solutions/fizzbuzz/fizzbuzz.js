9c6fff55576bc9e88f2b849ed5188b84bda2cbdd
Satyam Lal <ssatyamlal@gmail.com>
2024-10-12 21:05:44 +0530

    JS solution for fizzbuzz
    - Added JS solution for fizzbuzz
    - Please merge my solution as I am trying for hacktoberfest and this is my first time as an opensource contributor.
    - Trying to write other solutions in C language.

---
+++ solutions/fizzbuzz/fizzbuzz.js
@@ -0,0 +1,19 @@
+// Loop from 1 to 100
+for (let i = 1; i <= 100; i++) {
+    // Check if the number is divisible by both 3 and 5
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    }
+    // Check if the number is divisible by 3
+    else if (i % 3 === 0) {
+        console.log("Fizz");
+    }
+    // Check if the number is divisible by 5
+    else if (i % 5 === 0) {
+        console.log("Buzz");
+    }
+    // If none of the above conditions are true, print the number
+    else {
+        console.log(i);
+    }
+}
